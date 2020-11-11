f0ec5819e4b31981768f4f7240938fcb49051035
Yellsi <yellowm0098@gmail.com>
2020-11-11 02:19:28 +0200

    fixing errors on task11

--- task11.js
+++ task11.js
@@ -1,25 +1,24 @@
-function comp(str1, str2){
+function wordCompare(str1, str2){
 	str1 = str1.toLowerCase();
 	str2 = str2.toLowerCase();
 	var i = 0;
-	var s = str1.length;
-	var a = [];
+	var lengthOfFirstWord = str1.length;
+	var commonCharacters = [];
 	var j = 0;
-	var r = str2.length;
-	while(i < s)
+	var lengthOfSecondWord = str2.length;
+	while(i < lengthOfFirstWord)
 	{
-		while (j < r)
+		while (j < lengthOfSecondWord)
 		{
 
 			if (str1[i] == str2[j])
 			{
-				a.push(str2[j]);
+				commonCharacters.push(str2[j]);
 			}
 			j++;
 		
 		}
 		i++;
 	}
-	return a;
+	return commonCharacters;
 }
-
