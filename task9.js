cc323461d8624edbc9d9d926dca9a5b3fa04194b
Yellsi <yellowm0098@gmail.com>
2020-11-11 02:20:00 +0200

    fixing errors on task9

--- task9.js
+++ task9.js
@@ -1,13 +1,13 @@
 function sumOfNum(){
 	var i = 1;
-	var s;
-	var h;
+	var remaindersFromMultiplesOf3;
+	var remaindersFromMultiplesof5;
 	var sum = 0;
 	while (i < 1000)
 	{
-		s = i % 3;
-		h = i % 5;
-		if((s == 0) || (h == 0)){
+		remaindersFromMultiplesOf3 = i % 3;
+		remaindersFromMultiplesof5 = i % 5;
+		if((remaindersFromMultiplesOf3 == 0) || (remaindersFromMultiplesof5 == 0)){
 			sum += i;
 		}
 		i++;
