cb08df3daad4140f4550c581b06856f6cd48cb8c
Yellsi <yellowm0098@gmail.com>
2020-11-12 10:37:08 +0200

    fixed logic and now gives correct output

--- task8.js
+++ task8.js
@@ -1,15 +1,30 @@
-function toHAndM(num){
-	if (num > 60){
-		var hours = num / 60
+function toHoursAndMinutes(num){
+	if (num >= 60){
+		var hours = num / 60;
 		hours = Math.floor(hours);
 		var s = 60 * hours;
 		var minutes = num - s;
+		var j;
+		var h;
 		if (hours == 1){
-			console.log(hours + " hour, "+ minutes + " minutes");
+			h = hours + " hour ";
 		}else{
-			console.log(hours + " hours, " + minutes + " minutes.");
+			h = hours + " hours ";
+		} 
+
+		if (minutes > 1){
+			j = minutes + " minutes";
+		}else if (minutes == 1){
+			j = minutes + " minute";
 		}
+		else{
+			j = "";
+		}
+		console.log(h + j);
 	}else{
-		console.log(num + "minutes");
+		console.log(num + " minutes");
 	}
+		
+
 }
+toHoursAndMinutes(58);
