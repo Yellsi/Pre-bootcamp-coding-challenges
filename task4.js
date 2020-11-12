523d4cdc9358149ff8a688ff9fe9fbb13b085bf7
Yellsi <yellowm0098@gmail.com>
2020-11-12 10:34:20 +0200

    fixed fuction name

--- task4.js
+++ task4.js
@@ -1,20 +1,17 @@
-function myFunction(param1, param2){
-	var sum = param1 + param2;
+function ifNumbersHave3(number1, number2){
+	var sum = number1 + number2;
 	var sum1 = sum.toString();
 	var i = 0;
 	var j = sum1.length;
 	var s;
-	while(i < j){
-		if (sum1[i] == "3"){
-			s = true;
-		}else if ((3 == param1) || (param2 == 3)) {
-		
-			s = true;
-		}else{
-			s =  false;
-		}
-		i++;
-		}
-		return s;
-}
 
+	if (sum1.indexOf('3') > -1 )
+	{
+		return true;
+	}
+	else if ((3 == number1) || (number2 == 3)) {
+		return true;
+	}else{
+		return false;
+	}
+}
